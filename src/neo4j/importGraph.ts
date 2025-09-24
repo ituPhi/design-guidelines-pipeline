73707caf071c451bd22f929906e4f953568d1cbd
i2phi <jfiturbe@gmail.com>
2025-09-24 15:56:39 -0500

    add functio to read json and create ontology

---
+++ src/neo4j/importGraph.ts
@@ -0,0 +1,54 @@
+import neo4j from "neo4j-driver";
+import type { Success, ConceptFile } from "../types.ts";
+import enfoque from "../../data/json/enfoque.json" with { type: "json" };
+
+const config = {
+  url: process.env.NEO4J_URL as string,
+  username: process.env.NEO4J_USERNAME as string,
+  password: process.env.NEO4J_PASSWORD as string,
+};
+
+async function importGraph(file: ConceptFile): Promise<Success> {
+  const driver = neo4j.driver(
+    config.url,
+    neo4j.auth.basic(config.username, config.password),
+  );
+  const session = driver.session();
+  try {
+    await session.executeWrite(async (tx) => {
+      tx.run(
+        `
+         MERGE (c:Concept {id:$conceptId})
+         SET c.title = $title, c.description=$description, c.source_file=$file
+         FOREACH (chunk IN $chunks |
+           MERGE (ch:Chunk {id: chunk.id})
+           SET ch.title = chunk.title, ch.text = chunk.text, ch.file_ref=chunk.file_ref, ch.description=chunk.description
+           MERGE (c)-[:HAS]->(ch)
+           MERGE (ch)-[:PART_OF]->(c)
+         )
+         `,
+        {
+          conceptId: file.concept.id,
+          title: file.concept.title,
+          description: file.concept.description,
+          file: file.file,
+          chunks: file.concept.chunks,
+        },
+      );
+      return { message: "Graph has been imported successfully" };
+    });
+  } catch (error) {
+    console.error(error);
+    return { message: "Graph import failed" };
+  } finally {
+    await session.close();
+    await driver.close();
+    return { message: "Graph import completed" };
+  }
+}
+
+export async function runImport() {
+  const result = await importGraph(enfoque);
+  console.log(result);
+  return result;
+}
